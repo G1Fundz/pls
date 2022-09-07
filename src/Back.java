3725918356ff6e76a6f74df1677a89f95a663d66
G1Fundz <godwilljra@gmail.com>
2022-09-07 20:53:07 +0100

    Initial commit

---
+++ src/Back.java
@@ -0,0 +1,30 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+
+
+public class Back extends JFrame {
+
+    JFrame bFrame = new JFrame();
+
+    Back() {
+
+        setTitle("Back Workout");
+
+
+
+
+
+
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 500);
+        setLayout(null);
+        setVisible(true);
+        setResizable(false);
+
+    }
+}
+
